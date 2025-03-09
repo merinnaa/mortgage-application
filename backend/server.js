a2b45bc1f7d8d2f9ed51288276b5e48c873af1b6
mosszion <musdzion@gmail.com>
2025-03-09 16:48:32 -0400

    setup basic server config

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+//backend/server.js
+require('dotenv').config()
+const express = require("express");
+const cors = require('cors');
+
+
+const app = express();
+
+const port = 5000;
+
+//Enable CORs
+app.use(cors())
+
+//middleware to parse json bodies
+app.use(express.json());
+
+//Routes
+app.get('/', (req,res) => {
+  res.status(200).send(`Hello from the backend!!${process.env.message}`)
+});
+
+//start the server
+app.listen(port, () => {
+  console.log(`Backend running on port ${port}`)
+})
