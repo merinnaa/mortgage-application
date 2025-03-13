cb3edb5bc70197b3359988b8bf2ad3a29c5a9d51
mosszion <musdzion@gmail.com>
2025-03-13 18:47:59 -0400

    changed data

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,13 +1,12 @@
 -- Users table seeds here 
 INSERT INTO users (name, email, password, phone)
-VALUES ('Alice Johnson', 'alice.johnson@example.com', 'password123', '555-1234'),
-('Kira Lee', 'kira.lee@example.com', 'password456', '555-5678'),
-('Michael Smith', 'michael.smith@example.com', 'password789', '555-8765'),
-('Jessica Brown', 'jessica.brown@example.com', 'password101', '555-4321'),
-('Daniel White', 'daniel.white@example.com', 'password202', '555-1357'),
-('Sarah Johnson', 'sarah.johnson@example.com', 'password303', '555-2468'),
-('Ethan Williams', 'ethan.williams@example.com', 'password404', '555-3579'),
-('Olivia Davis', 'olivia.davis@example.com', 'password505', '555-4680'),
-('James Wilson', 'james.wilson@example.com', 'password606', '555-5791'),
-('Emily Taylor', 'emily.taylor@example.com', 'password707', '555-6802'),
-('Ghidion Mengesteab', 'ghidionmengesetab6@gmail.com', 'ghidu1234', '666-4325');
+VALUES ('Mussie', 'mussie@example.com', 'password123', '555-1234'),
+('Chloe', 'chloe@example.com', 'password456', '555-5678'),
+('Ashley', 'ashley@example.com', 'password789', '555-8765'),
+('Luwam', 'luwam@example.com', 'password101', '555-4321'),
+('Daniel', 'daniel@example.com', 'password202', '555-1357'),
+('Samuel', 'samuel@example.com', 'password303', '555-2468'),
+('Ladi', 'ladi@example.com', 'password404', '555-3579'),
+('Monis', 'monis@example.com', 'password505', '555-4680'),
+('Ayock', 'ayock@example.com', 'password606', '555-5791');
+
