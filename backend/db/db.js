56e19b5272fa1ccf285340d3a925a4e5adb965ce
mosszion <musdzion@gmail.com>
2025-03-17 17:25:01 -0400

    added ssl certification

--- backend/db/db.js
+++ backend/db/db.js
@@ -7,7 +7,10 @@ const dbParams = {
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // If your database uses a self-signed certificate
+  }
 };
 
 const db = new Pool(dbParams)
