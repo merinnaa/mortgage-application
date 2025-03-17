a78ac8711da54ea8e3b8e23b53aa49862a961e4d
luwam <lwamyohannes2018@gmail.com>
2025-03-17 08:13:50 -0600

    modified:   frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>mortgage-application</h1>
+     <div className="App">
+      {/* <h1>mortgage-application</h1> */}
     </div>
   );
 }
