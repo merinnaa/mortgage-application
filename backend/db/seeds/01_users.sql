73591ad7020d17300e279cf175a2cdb49f9072bc
mosszion <musdzion@gmail.com>
2025-03-21 19:57:04 -0400

    schema and seeds modified

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,12 +1,12 @@
 -- Users table seeds here 
-INSERT INTO users (name, email, password, phone)
-VALUES ('Mussie', 'mussie@example.com', 'password123', '555-1234'),
-('Chloe', 'chloe@example.com', 'password456', '555-5678'),
-('Ashley', 'ashley@example.com', 'password789', '555-8765'),
-('Luwam', 'luwam@example.com', 'password101', '555-4321'),
-('Daniel', 'daniel@example.com', 'password202', '555-1357'),
-('Samuel', 'samuel@example.com', 'password303', '555-2468'),
-('Ladi', 'ladi@example.com', 'password404', '555-3579'),
-('Monis', 'monis@example.com', 'password505', '555-4680'),
-('Ayock', 'ayock@example.com', 'password606', '555-5791');
+INSERT INTO users (name, email, password)
+VALUES ('Mussie', 'mussie@example.com', 'password123'),
+('Chloe', 'chloe@example.com', 'password456'),
+('Ashley', 'ashley@example.com', 'password789'),
+('Luwam', 'luwam@example.com', 'password101'),
+('Daniel', 'daniel@example.com', 'password202'),
+('Samuel', 'samuel@example.com', 'password303'),
+('Ladi', 'ladi@example.com', 'password404'),
+('Monis', 'monis@example.com', 'password505'),
+('Ayock', 'ayock@example.com', 'password606');
 
