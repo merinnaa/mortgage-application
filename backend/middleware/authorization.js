5e6cc307fb12b77ac9924076ea3e10ff175edd76
mosszion <musdzion@gmail.com>
2025-03-21 18:59:33 -0400

    added authorization logic

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,26 @@
+//import libraries 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+
+module.exports = async (req, res, next) =>{
+  try {
+    //1.destructure the token
+    const jwtToken = req.headers("token");
+      if(!jwtToken) {
+        return res.status(403).json("Not Authorized")
+      }
+
+    const payload = jwt.verify(jwtToken, process.env.JWT_SECRET)
+
+    req.user = payload.user;
+
+    next();
+
+    
+  } catch (error) {
+    console.error(error.message)
+    return res.status(403).json("Not Authorized")
+    
+  }
+}
