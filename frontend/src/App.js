07511491de8945419242af376dd9102a0d9746b5
mosszion <musdzion@gmail.com>
2025-03-22 03:24:56 -0400

    added routes to react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
-
+import React, {Fragment, useState} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+
+
+//import components
+import Login from "./components/Login"
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean)
+  }
   return (
-    <div className="App">
-      <h1>mortgage-application</h1>
-    </div>
+    <Fragment>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path="/login" element= {< Login  setAuth={setAuth}/> }/>
+            <Route path="/register" element= {< Register  setAuth={setAuth}/> }/>
+            <Route path="/dashboard" element= {< Dashboard  setAuth={setAuth}/> }/>
+
+          </Routes>
+
+        </div>
+      </Router>
+
+    </Fragment>
+
   );
 }
 
