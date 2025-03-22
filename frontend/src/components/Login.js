38f32e672c9318672a0ece3dfcd4b1265bd173db
mosszion <musdzion@gmail.com>
2025-03-22 03:53:34 -0400

    set the input form and fetch login route from backend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,72 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 
 
 const Login = ({setAuth})=> {
+  const [inputs, setInputs] = useState({
+    email:"",
+    password:""
+  })
+
+  const {email, password} = inputs;
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value});
+  }
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = {email, password};
+
+      const response = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers : {"Content-Type" : "application/json"},
+        body: JSON.stringify(body)
+      });
+
+      const parseRes = await response.json();
+
+      if(parseRes.token) {
+        localStorage.setItem("token", parseRes.token);
+        setAuth(true);
+        
+      }else {
+        setAuth(false)
+        
+      }
+      
+    } catch (error) {
+      console.error(error.message)
+      
+    }
+  }
+  
+
   return (
     <Fragment>
       <h1>Login</h1>
+      <form onSubmit = {onSubmitForm}>
+        <input 
+          type="email"
+          name = "email"
+          placeholder="email"
+          className="form-control my-3"
+          value={email}
+          onChange={(e) => onChange(e)}
+
+        />
+        <input 
+        type="password"
+        name = "password"
+        placeholder="password"
+        className="form-control my-3"
+        value={password}
+        onChange={(e) => onChange(e)}
+        
+        />
+
+       <button className="btn btn-success btn-block">Login</button>
+      </form>
     </Fragment>
   )
 
