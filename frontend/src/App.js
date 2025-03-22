635d031a957f3d7a4f88144f4c9d0211a6fdeea8
mosszion <musdzion@gmail.com>
2025-03-22 04:01:40 -0400

    fetched token , created function to check authenticity of routes, passed toast

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState , useEffect} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -15,10 +18,31 @@ function App() {
   const setAuth = (boolean) => {
     setIsAuthenticated(boolean)
   }
+  async function isAuth() {
+    try {
+      const response = await fetch ("http://localhost:5000/auth/is-verify", {
+        method: "GET",
+        headers: {token: localStorage.token}
+      })
+
+      const parseRes = await response.json();
+
+      parseRes === true ? setIsAuthenticated(true) :setIsAuthenticated(false);
+      
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
+  useEffect(() => {
+    isAuth()
+  }, [])
+
   return (
     <Fragment>
       <Router>
         <div className='container'>
+          <ToastContainer/>
           <Routes>
             <Route path="/login" element= { !isAuthenticated ? < Login  setAuth={setAuth}/> : <Navigate to="/dashboard"/> }/>
             <Route path="/register" element= {!isAuthenticated ? < Register  setAuth={setAuth}/> : <Navigate to="/login"/> }/>
