224eefaffde1e236459cc22144d87cf109ee6028
mosszion <musdzion@gmail.com>
2025-03-22 21:36:53 -0400

    renamed to index

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+//backend/server.jsf
+
+ // Your server code here
+ require("dotenv").config()
+ const express = require("express");
+ const cors = require('cors');
+ 
+ 
+ const app = express();
+ const port = process.env.PORT || 4000;
+ 
+ 
+ 
+ //middleware to parse json bodies
+ app.use(express.json());
+ app.use(express.urlencoded({extended: true,}))
+ app.use(cors())
+ 
+ /////////////////////////////////////////////////////
+ //Routes
+ ////////////////////////////////////////////////////
+ 
+ // Register and Login routes
+ 
+ app.use("/auth", require("./routes/jwtAuth"));
+ 
+ // dashboard route
+ app.use("/dashboard", require("./routes/dashboard"));
+ 
+ //start the server
+ app.listen(port, () => {
+   console.log(`Backend running on port ${port}`)
+ })
+
