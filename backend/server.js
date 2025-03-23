de4130d907a38e21e3ce2837362c181e55886b42
mosszion <musdzion@gmail.com>
2025-03-22 21:35:18 -0400

    renamed server.js to index, modified json file

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,34 @@
 //backend/server.jsf
 
-require("dotenv").config()
-const express = require("express");
-const cors = require('cors');
+ // Your server code here
+ require("dotenv").config()
+ const express = require("express");
+ const cors = require('cors');
+ 
+ 
+ const app = express();
+ const port = process.env.PORT || 4000;
+ 
+ 
+ 
+ //middleware to parse json bodies
+ app.use(express.json());
+ app.use(express.urlencoded({extended: true,}))
+ app.use(cors())
+ 
+ /////////////////////////////////////////////////////
+ //Routes
+ ////////////////////////////////////////////////////
+ 
+ // Register and Login routes
+ 
+ app.use("/auth", require("./routes/jwtAuth"));
+ 
+ // dashboard route
+ app.use("/dashboard", require("./routes/dashboard"));
+ 
+ //start the server
+ app.listen(port, () => {
+   console.log(`Backend running on port ${port}`)
+ })
 
-console.log(process.env)
-const app = express();
-const port = process.env.PORT || 4000;
-
-
-const port = process.env.PORT
-
-
-
-
-//middleware to parse json bodies
-app.use(express.json());
-app.use(express.urlencoded({extended: true,}))
-app.use(cors())
-
-/////////////////////////////////////////////////////
-//Routes
-
-////////////////////////////////////////////////////
-
-// Register and Login routes
-
-app.use("/auth", require("./routes/jwtAuth"));
-
-// dashboard route
-app.use("/dashboard", require("./routes/dashboard"));
-
-app.get('/', (req,res) => {
-  res.status(200).send(`Hello from the backend!!${process.env.MESSAGE}`)
-});
-
-
-//start the server
-app.listen(port, () => {
-  console.log(`Backend running on port ${port}`)
-})
