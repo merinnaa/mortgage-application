7515b1f483e509b75dd99e7d0cd74126f17565bf
mosszion <musdzion@gmail.com>
2025-03-23 20:08:56 -0400

    added component for front page

---
+++ frontend/src/components/Front.js
@@ -0,0 +1,39 @@
+import React,{Fragment} from "react"
+import { Link } from "react-router-dom";
+import '../App.css';
+
+const Front = () => {
+  return (
+    <Fragment >
+      <h1 class="front">Fundify</h1>
+      <div class="imagesContainer">
+        
+        <div class="imageSample">
+          <img src="/himgs/house1.png"/>
+        </div>
+        <div class="imageSample">
+          <img src="/himgs/house2.png"/>
+        </div>
+        <div class="imageSample">
+          <img src="/himgs/house3.png"/>
+        </div>
+        <div class="imageSample">
+          <img src="/himgs/house4.png"/>
+        </div>
+      
+       
+
+   
+
+      </div>
+
+      <Link to="/login"> Login </Link>
+
+      <Link to="/register"> SignUp </Link>
+      
+
+    </Fragment>
+  )
+
+};
+export default Front;
