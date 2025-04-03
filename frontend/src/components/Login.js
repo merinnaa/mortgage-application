f2f3d15e1cd5a7653bbfe68d817bd78962f539c6
mosszion <musdzion@gmail.com>
2025-04-02 21:55:44 -0400

    added google login element

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState} from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import {GoogleLogin} from '@react-oauth/google'
 
 
 const Login = ({setAuth})=> {
@@ -45,6 +46,37 @@ const Login = ({setAuth})=> {
     }
   }
   
+  // Google login success handler
+  const googleSuccess = async (response) => {
+    try {
+      const tokenId = response.tokenId;
+
+      const responseGoogle = await fetch("https://mortgage-application-server.vercel.app/google-login/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ tokenId }),
+      });
+
+      const parseRes = await responseGoogle.json();
+
+      if (parseRes.token) {
+        localStorage.setItem("token", parseRes.token);
+        setAuth(true);
+        toast.success("Google Login Success!");
+      } else {
+        setAuth(false);
+        toast.error(parseRes);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  // Google login error handler
+  const googleFailure = (error) => {
+    toast.error("Google Login Failed. Try Again!");
+  };
+
 
   return (
     <Fragment>
@@ -71,6 +103,14 @@ const Login = ({setAuth})=> {
 
        <button className="btn btn-success btn-block">Login</button>
       </form>
+      {/* Google Login Button */ }
+      <GoogleLogin
+        clientId="445644859415-32ejmapv6q579590duc4oniuv8saethp.apps.googleusercontent.com"  // Google Client ID
+        buttonText="Login with Google"
+        onSuccess={googleSuccess}
+        onFailure={googleFailure}
+        cookiePolicy={'single_host_origin'}
+      />
       <Link to="/register">Register</Link>
     </Fragment>
   )
