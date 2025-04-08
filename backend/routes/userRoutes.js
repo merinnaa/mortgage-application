4f3bcbcbfd1efa5b191fe89aaa58a29525f4b292
mosszion <musdzion@gmail.com>
2025-04-08 07:36:11 -0400

    renamed user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,23 @@
+// import libraries
+
+const router = require("express").Router();
+const validInfo = require("../middleware/validInfo");
+const users = require("../controllers/userController")
+const authorization = require("../middleware/authorization")
+
+
+
+// First time registering a user
+/////////////////////////////////////////////////////////////////////////////////////////
+router.post("/register",validInfo,users.register)
+
+// login route
+/////////////////////////////////////////////////////////////////////////////////////////
+router.post("/login", validInfo, users.login)
+
+// verify route
+/////////////////////////////////////////////////////////////////////////////////////
+router.get("/is-verify" , authorization,users.verify)
+
+
+module.exports = router
