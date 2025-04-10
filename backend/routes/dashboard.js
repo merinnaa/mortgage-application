ef694473347aededef9b6be5caa5abed5bc11cd1
mosszion <musdzion@gmail.com>
2025-04-10 02:55:21 -0400

    added lender get route

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const usersDb = require("../db/queries/users");
+const lenderDb = require("../db/queries/lenders")
 const authorization = require("../middleware/authorization");
 
 router.get("/",authorization, async (req,res) => {
@@ -11,6 +12,16 @@ router.get("/",authorization, async (req,res) => {
     console.error(error.message);
     res.status(500).json("Server Error")
     
+  }
+})
+
+router.get("/lender",authorization, async(req,res) => {
+  try {
+    const lender = await lenderDb.getLendersById(req.user)
+      res.json(lender.rows[0])
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server Error")
   }
 })
 
