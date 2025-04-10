4f74e9321f394889bd4522611737992a41a38d1a
mosszion <musdzion@gmail.com>
2025-04-09 23:54:24 -0400

    modified lenders query

---
+++ backend/db/queries/lenders.js
@@ -0,0 +1,22 @@
+const db = require('../db');
+
+//get users by email
+const getLendersByEmail = (email) => {
+  return db.query('SELECT * FROM users WHERE email=$1;',[email])
+  
+      
+            
+}
+//Get users by id 
+const getLendersById = (id) => {
+  return db.query('SELECT * FROM users WHERE id=$1;',[id])
+      
+            
+}
+
+const createNewLender = (first_name,last_name,email,bcryptPassword,institution,employee_id,role) => {
+  return db.query("INSERT INTO users (first_name,last_name,email, password, institution, employee_id, role) VALUES ($1, $2 , $3,$4,$5,$6,$7) RETURNING *;",[first_name,last_name, email, bcryptPassword,institution,employee_id,role] )
+  
+}
+
+module.exports = {getLendersByEmail,createNewLender,getLendersById}
