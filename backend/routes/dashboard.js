f6fe05b4959808de8c0c041f02ba49e966be7fc5
mosszion <musdzion@gmail.com>
2025-04-10 16:59:37 -0400

    add routes of lenders and users and added lender roles

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,31 +1,33 @@
 const router = require("express").Router();
 const usersDb = require("../db/queries/users");
-const lenderDb = require("../db/queries/lenders")
+const lendersDb = require("../db/queries/lenders");
 const authorization = require("../middleware/authorization");
 
-router.get("/",authorization, async (req,res) => {
+router.get("/", authorization, async (req, res) => {
   try {
-    const user = await usersDb.getUsersById(req.user)
-      res.json(user.rows[0])
+ 
+    const lender = await lendersDb.getLenderByIdEmail(req.id, req.email);
+    const user = await usersDb.getUsersById(req.id);
+  
+    if (lender.rows.length !==0) {    
+      
+      if(lender.rows[0].role === 'admin'){
+        console.log(lender.rows[0])
+        return res.json("This is admin page")
+      }    
 
+        return res.json("This is Supervisior")     
+      
+    }
+    if (user.rows.length !== 0) {
+      console.log(user.rows[0])    
+      return res.json(user.rows[0])
+    }
   } catch (error) {
     console.error(error.message);
-    res.status(500).json("Server Error")
-    
+    res.status(500).json("Server Error");
   }
-})
-
-router.get("/lender",authorization, async(req,res) => {
-  try {
-    const lender = await lenderDb.getLendersById(req.user)
-      res.json(lender.rows[0])
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json("Server Error")
-  }
-})
-
-
+});
 
 
 module.exports = router;
