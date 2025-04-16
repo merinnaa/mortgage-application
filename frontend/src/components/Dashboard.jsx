e98159bb7fcc20407ae920fee12f44ae54c29b3b
mosszion <musdzion@gmail.com>
2025-04-16 12:15:40 -0400

    added backend routes to dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -34,15 +34,19 @@ const Dashboard = () => {
     }
 
     const formData = new FormData();
-    formData.append("file", idFile);
-    formData.append("file", bankFile);
-    formData.append("file", incomeFile);
-
+    formData.append("id", idFile);
+    formData.append("bank", bankFile);
+    formData.append("income", incomeFile);
+       
     try {
       const response = await fetch("https://mortgage-application-server.vercel.app/upload", {
         method: "POST",
+        headers: {
+          token: localStorage.token 
+        },
         body: formData
       });
+
 
       const result = await response.json();
       if (response.ok) {
