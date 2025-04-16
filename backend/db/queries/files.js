7d8df1c3b0596ca961844dcf6b1b8288345a73aa
mosszion <musdzion@gmail.com>
2025-04-16 12:17:23 -0400

    added queries for id , bank and income tables

--- backend/db/queries/files.js
+++ backend/db/queries/files.js
@@ -1,9 +1,32 @@
 const db = require('../db');
 
-//get lenders by email
-const getFile = (user_id,filename,mimetype, size, file_data) => {
-  return db.query('INSERT INTO upload_files(user_id,filename, mimetype, size, file_data) VALUES ($1,$2,$3,$4,$5)',[user_id,filename,mimetype, size, file_data])     
-            
-}
 
-module.exports={getFile}
+/**
+ * Save file to the appropriate table based on the field name (not the filename)
+ * @param {string} user_id 
+ * @param {string} field - 'id', 'bank', or 'income'
+ * @param {string} filename 
+ * @param {string} mimetype 
+ * @param {number} size 
+ * @param {Buffer} file_data 
+ */
+const getFile = (user_id, field, filename, mimetype, size, file_data) => {
+  let table;
+
+  if (field === 'id') {
+    table = 'id';
+  } else if (field === 'bank') {
+    table = 'bank';
+  } else if (field === 'income') {
+    table = 'income';
+  } else {
+    throw new Error(`Invalid field name: ${field}`);
+  }
+
+  const query = `INSERT INTO ${table} (user_id, filename, mimetype, size, file_data)
+                 VALUES ($1, $2, $3, $4, $5)`;
+
+  return db.query(query, [user_id, filename, mimetype, size, file_data]);
+};
+
+module.exports = { getFile };
