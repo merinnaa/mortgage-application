29132d6c7567df3d84ba933362cb8e3624721d84
mosszion <musdzion@gmail.com>
2025-04-17 19:51:35 -0400

    added thankyou page after registration

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -60,7 +60,7 @@ setErrors({})
       localStorage.setItem("token", parseRes.token);
       setAuth(true);
      toast.success("Registered Successfully")
-     navigate("/dashboard")  
+     navigate("/thankYou")  
     }else {
       setAuth(false)
      toast.error(parseRes)
