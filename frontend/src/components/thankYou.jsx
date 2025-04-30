60db234cd14d944ff4cabc1b6889ae27d45eae60
mosszion <musdzion@gmail.com>
2025-04-30 19:04:31 -0400

    renamed thankyou page

--- frontend/src/components/thankYou.jsx
+++ frontend/src/components/thankYou.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 const ThankYou = () => {
+  
   return (
     <>
       <div className="w-full h-[982px] relative bg-stone-100 overflow-hidden">
